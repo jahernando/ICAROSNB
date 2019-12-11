d9e99f3762d4fb3125063858e2a813ea35c57b0b
Jose A. Hernando <hernando.joseangel@gmail.com>
2019-12-11 17:26:13 +0100

    clean

--- JAH/detsim/detsim.py
+++ JAH/detsim/detsim.py
@@ -1,3 +1,16 @@
+#
+#   DetSim Functionality: From energy deposits to raw-waveforms (pes)
+#
+#   GM, JAH, Dec 2019
+#
+
+#
+#
+#
+#
+#
+
+
 import time
 
 import numpy             as np
@@ -13,16 +26,38 @@ import invisible_cities.core    .system_of_units_c as system_of_units
 import invisible_cities.core    .fit_functions     as fitf
 import invisible_cities.database.load_db           as db
 
-import myhistos     as ht
-import voxels       as vx
-
 import matplotlib.pyplot as plt
+
+units = system_of_units.SystemOfUnits()
+
+#
+# Utilities
+#----------------
+
+def bincounter(xs, dx = 1., x0 = 0.):
+    ixs    = ((xs + x0) // dx).astype(int)
+    return np.unique(ixs, return_counts=True)
+
+
+def bincounterdd(xxs, dxs = 1., x0s = 0., n = 1000):
+
+    xs = np.array(xxs, dtype = float).T
+    dxs = np.ones_like(xs) * dxs
+    x0s = np.ones_like(xs) * x0s
+
+    ixs = ((xs - x0s) // dxs).astype(int)
+    ids, ccs =  np.unique(ixs, axis = 0, return_counts = True)
+    return ids.T, ccs
+
+
+def bins_edges(centers, dx):
+    bins = centers - 0.5 * dx
+    return np.append(bins, bins[-1] + dx)
 
 #
 # Configuration
 #-------------------
 
-units = system_of_units.SystemOfUnits()
 
 class Singleton(object):
 
@@ -34,22 +69,11 @@ class Singleton(object):
         return cls._instance
 
 
-# class WF:
-#
-#      def __init__(self, buffer_size, bin_size, nsensors):
-#          self.buffer_size  = buffer_size
-#          self.bin_size     = bin_size
-#          self.nsensors     = nsensors
-#          self.nbins        = int(buffer // time_bin)
-#          self.bins         = np.arange(self.nbins) *  bin_size
-#          self.nsensors     = nsensors
-#          self._wf           = np.zeros(self.nbins, nsensors)
-#
-#      def wf(self):
-#          return np.copy(self.wf)
-
-
 class DetSimParameters(Singleton):
+    """ Singleton with the configuration and NEXT detector parameters
+    control parameters are: pmt and SiPM wf time bins,
+    and re-voxelization size (mm) of the diffused electrons
+    """
 
     def __init__(self):
 
@@ -59,26 +83,25 @@ class DetSimParameters(Singleton):
         self.conde_policarpo_factor = 0.10
 
         self.drift_velocity         = 1.00 * units.mm / units.mus
-        self.lifetime               = 8.e5 * units.mus
+        self.lifetime               = 5.e3 * units.ms
 
         self.transverse_diffusion   = 1.00 * units.mm / units.cm**0.5
         self.longitudinal_diffusion = 0.30 * units.mm / units.cm**0.5
-        self.voxel_sizes            = 2.00 * units.mm, 2.00 * units.mm, 0.2 * units.mm
+        self.voxel_sizes            = np.array((2.00 * units.mm, 2.00 * units.mm, 0.2 * units.mm), dtype = float)
 
         self.el_gain                = 1e3
         self.el_gain_sigma          = np.sqrt(self.el_gain * self.conde_policarpo_factor)
+
+        self.wf_buffer_time         = 1000 * units.mus
+        self.wf_pmt_bin_time        =   25 * units.ns
+        self.wf_sipm_bin_time       =    1 * units.mus
 
         self.EP_z                   = 632 * units.mm
         self.EL_dz                  =  5  * units.mm
         self.TP_z                   = -self.EL_dz
         self.EL_dtime               =  self.EL_dz / self.drift_velocity
-        self.EL_pmt_time_sample     = 200 * units.ns
-        self.EL_sipm_time_sample    =   1 * units.mus
-
-
-        self.wf_buffer_time         = 1000 * units.mus
-        self.wf_pmt_bin_time        =   25 * units.ns
-        self.wf_sipm_bin_time       =    1 * units.mus
+        self.EL_pmt_time_sample     =  self.wf_pmt_bin_time
+        self.EL_sipm_time_sample    =  self.wf_sipm_bin_time
 
         self.npmts                  = 12
         self.nsipms_side            = 22
@@ -100,8 +123,8 @@ class DetSimParameters(Singleton):
         self.x_sipms  = np.array([sipm[0] for sipm in self.sipms])
         self.y_sipms  = np.array([sipm[1] for sipm in self.sipms])
 
-        self.xybins   = vx.bins_edges(np.sort(self.x_sipms), 0.5 * self.sipm_pitch), \
-                        vx.bins_edges(np.sort(self.y_sipms), 0.5 * self.sipm_pitch)
+        self.xybins   = bins_edges(np.sort(self.x_sipms), 0.5 * self.sipm_pitch), \
+                        bins_edges(np.sort(self.y_sipms), 0.5 * self.sipm_pitch)
 
 
     def configure(self, **kargs):
@@ -116,32 +139,50 @@ detsimparams = DetSimParameters()
 # Utilities
 #----------------
 
-def _array(xs, ns):
-    return np.concatenate([np.repeat(xi, ni) for xi, ni in zip(xs, ns)])
+def bincounter(xs, dx = 1., x0 = 0.):
+    ixs    = ((xs + x0) // dx).astype(int)
+    return np.unique(ixs, return_counts=True)
 
+
+def bincounterdd(xxs, dxs = 1., x0s = 0., n = 1000):
+
+    xs = np.array(xxs, dtype = float).T
+    dxs = np.ones_like(xs) * dxs
+    x0s = np.ones_like(xs) * x0s
+
+    ixs = ((xs - x0s) // dxs).astype(int)
+    ids, ccs =  np.unique(ixs, axis = 0, return_counts = True)
+    return ids.T, ccs
+
+
+def bins_edges(centers, dx):
+    bins = centers - 0.5 * dx
+    return np.append(bins, bins[-1] + dx)
 #
 #  Secondary electrons
 #------------------------
 
 def generate_deposits(x0 = 0., y0 = 0., z0 = 300. * units.mm,
                       xsigma = 1.00 * units.mm, size = 1853, dx = 1.00 * units.mm):
+    """ generate energy deposits, dummy funciton
+    """
 
     xs = np.random.normal(x0, xsigma, size)
     ys = np.random.normal(y0, xsigma, size)
     zs = np.random.normal(z0, xsigma, size)
 
-    xbins = vx.equal_size_bins(np.min(xs), np.max(xs), dx)
-    ybins = vx.equal_size_bins(np.min(ys), np.max(ys), dx)
-    zbins = vx.equal_size_bins(np.min(zs), np.max(zs), dx)
-
-    vxs, vys, vzs, vnes = vx.voxels3d(xs, ys, zs, xbins, ybins, zbins)
+    xpos, vnes = bincounterdd((xs, ys, zs), dx)
+    vxs, vys, vzs = dx * xpos
     vnes = detsimparams.wi * vnes
+
     return (vxs, vys, vzs, vnes)
 
 
 def generate_electrons(energies    : np.array,
                        wi          : float = detsimparams.wi,
                        fano_factor : float = detsimparams.fano_factor) -> np.array:
+    """ generate number of electrons starting from energy deposits
+    """
     nes  = np.array(energies/wi, dtype = int)
     pois = nes < 10
     nes[ pois] = np.random.poisson(nes[pois])
@@ -153,6 +194,8 @@ def drift_electrons(zs             : np.array,
                     electrons      : np.array,
                     lifetime       : float = detsimparams.lifetime,
                     drift_velocity : float = detsimparams.drift_velocity) -> np.array:
+    """ returns number of electrons due to lifetime loses from initial secondary electrons
+    """
     ts  = zs / drift_velocity
     nes = np.copy(electrons -np.random.poisson(electrons * (1. - np.exp(-ts/lifetime))))
     nes[nes < 0] = 0
@@ -165,45 +208,59 @@ def diffuse_electrons(xs                     : np.array,
                       electrons              : np.array,
                       transverse_diffusion   : float = detsimparams.transverse_diffusion,
                       longitudinal_diffusion : float = detsimparams.longitudinal_diffusion,
-                      voxel_sizes            : Tuple = detsimparams.voxel_sizes) -> Tuple:
+                      voxel_sizes            : np.array = detsimparams.voxel_sizes) -> Tuple:
+    """
+    starting from a voxelized electrons with positions xs, ys, zs, and number of electrons, electrons,
+    apply diffusion and return voxelixed electrons with positions xs, ys, zs, an electrons
+    the voxel_size arguement controls the size of the voxels for the diffused electrons
+    """
     nes = electrons
-    xs = _array(xs, nes); ys = _array(ys, nes); zs = _array(zs, nes)
+    xs = np.repeat(xs, nes); ys = np.repeat(ys, nes); zs = np.repeat(zs, nes)
     #zzs = np.concatenate([np.array((zi,)*ni) for zi, ni in zip(zs, ns)])
     sqrtz = zs ** 0.5
     vxs  = np.random.normal(xs, sqrtz * transverse_diffusion)
     vys  = np.random.normal(ys, sqrtz * transverse_diffusion)
     vzs  = np.random.normal(zs, sqrtz * longitudinal_diffusion)
     vnes = np.ones(vxs.size)
-    dx, dy, dz = voxel_sizes
-    xbins = vx.equal_size_bins(np.min(vxs), np.max(vxs), dx)
-    ybins = vx.equal_size_bins(np.min(vys), np.max(vys), dy)
-    zbins = vx.equal_size_bins(np.min(vzs), np.max(vzs), dz)
-    vxs, vys, vzs, vnes = vx.voxels3d(vxs, vys, vzs, xbins, ybins, zbins)
+    vpos, vnes = bincounterdd((vxs, vys, vzs), voxel_sizes)
+    vxs, vys, vzs = voxel_sizes[:, np.newaxis] * vpos
     return (vxs, vys, vzs, vnes)
 
 #
-#  EL
-#-----------
+#  EL photons and pes at sensors
+#---------------------------------
 
-def psf(dx, dy, dz, factor = 1.):
+def _psf(dx, dy, dz, factor = 1.):
+    """ generic analytic PSF function
+    """
     return factor * np.abs(dz) / (2 * np.pi) / (dx**2 + dy**2 + dz**2)**1.5
 
-psf_pmt  = lambda dx, dy : psf(dx, dy, detsimparams.EP_z, factor = 25e3)
 
-psf_sipm = lambda dx, dy : psf(dx, dy, detsimparams.TP_z, factor = 10.)
+psf_pmt  = lambda dx, dy : _psf(dx, dy, detsimparams.EP_z, factor = 25e3)
+
+psf_sipm = lambda dx, dy : _psf(dx, dy, detsimparams.TP_z, factor = 10.)
+
 
 def generate_EL_photons(electrons      : np.array,
                         el_gain        : float = detsimparams.el_gain,
                         el_gain_sigma  : float = detsimparams.el_gain_sigma):
+    """ generate number of EL-photons produced by secondary electrons that reach
+    the EL (after drift and diffusion)
+    """
     nphs      = electrons * np.random.normal(el_gain, el_gain_sigma, size = electrons.size)
     return nphs
 
-def estimate_pes_at_sensors(xs        : np.array,
-                            ys        : np.array,
-                            x_sensors : np.array,
-                            y_sensors : np.array,
-                            photons   : np.array,
+
+def estimate_pes_at_sensors(xs        : np.array,  # nelectrons
+                            ys        : np.array,  # nelectrons
+                            photons   : np.array,  # nelectrons
+                            x_sensors : np.array,  # nsensors
+                            y_sensors : np.array,  # nsensors
                             psf       : Callable) -> np.array:
+    """ estimate the number of pes at sensors from the number of photons
+    produced by electrons that reach the EL at xs, ys positions.
+    It returns an array of pes with nelectrons x nsensors shape.
+    """
     dxs     = xs[:, np.newaxis] - x_sensors
     dys     = xs[:, np.newaxis] - y_sensors
     photons = photons[:, np.newaxis] + np.zeros_like(x_sensors)
@@ -212,52 +269,60 @@ def estimate_pes_at_sensors(xs        : np.array,
     #pes = np.random.poisson(pes)
     return pes
 
+
 def estimate_pes_at_pmts(xs      : np.array,
                          ys      : np.array,
                          photons : np.array) -> np.array:
-    return estimate_pes_at_sensors(xs, ys, detsimparams.x_pmts, detsimparams.y_pmts, photons, psf_pmt)
+    return estimate_pes_at_sensors(xs, ys, photons, detsimparams.x_pmts, detsimparams.y_pmts, psf_pmt)
 
 
 def estimate_pes_at_sipms(xs      : np.array,
                           ys      : np.array,
                           photons : np.array) -> np.array:
-    return estimate_pes_at_sensors(xs, ys, detsimparams.x_sipms, detsimparams.y_sipms, photons, psf_sipm)
+    return estimate_pes_at_sensors(xs, ys, photons, detsimparams.x_sipms, detsimparams.y_sipms, psf_sipm)
 
 #
 #  WFs
 #-----------
 
-def create_wfs(ts             : np.array, # nevents
-               pes            : np.array, # nevents x nsensors
+
+def create_wfs(ts             : np.array, # nelectrons
+               pes            : np.array, # (nelectrons, nsensors)
                wf_buffer_time : float = detsimparams.wf_buffer_time,
                wf_bin_time    : float = detsimparams.wf_pmt_bin_time,
                el_time_sample : float = detsimparams.EL_pmt_time_sample,
                el_time        : float = detsimparams.EL_dtime):
+    """ create the wfs starting drom the pes produced per electrons and each sensor
+    the control parameters are the wf_bin_time and the el_time_sample,
+    by default they are the same parameters.
+    The number of pes photos per sensor are spread along the EL-time depending
+    on the wf_bin_time
+    Returns: time bins of the wf (nbins), and wfs contents (nbins, nsensors)
+    """
 
     nsize = int(el_time        // el_time_sample)
     nbins = int(wf_buffer_time // wf_bin_time )
 
-    wftimes = np.arange(nbins + 1) * wf_bin_time
+    wftimes = wf_bin_time    * np.arange(nbins)
     dts     = el_time_sample * np.arange(nsize)
 
-    def _times(its, ipes):
-        nts  = its[:, np.newaxis] + dts
-        nns   = np.random.poisson(ipes / nsize, size = (nsize, ipes.size))
-        return np.repeat(nts.flatten(), nns.T.flatten())
+    nsensors  = 1 if len(pes.shape) == 1 else int(pes.shape[1])
+    wfs     = np.zeros((nbins, nsensors), dtype = int)
 
-    def _wf(itimes):
-        idtimes     = (itimes // wf_bin_time).astype(int)
-        ids, counts = np.unique(idtimes, return_counts=True)
-        iwf         = np.zeros(nbins + 1)
-        iwf[ids]    = counts
+    def _wf(its, ipes, iwf):
+        # for each sensor, check if has pes, sample pes in EL times and fill wf
+        if (np.sum(ipes) <= 0): return iwf
+
+        nts        = np.repeat(its, ipes.astype(int))
+        sits, spes = bincounter(nts, wf_bin_time)
+
+        spesn       = np.random.poisson(spes/nsize, size = (nsize, spes.size))
+        for k, kpes in enumerate(spesn):
+            kk =  int(k * el_time_sample / wf_bin_time)
+            iwf[sits + kk] = iwf[sits + kk] + kpes
         return iwf
 
-    # initial version
-    #def _wf(itimes):
-    #    c, _ = np.histogram(itimes, bins = wftimes)
-    #    return c
-
-    wfs =  np.array([_wf(_times(ts, ipes)) for ipes in pes.T])
+    [_wf(ts, ipes, iwf) for ipes, iwf in zip(pes.T, wfs.T)]
 
     return wftimes, wfs
 
@@ -276,18 +341,35 @@ def create_wfs_sipms(ts, pes,
 #-----------------
 
 def histo(var, name = '', nbins = 100):
-    ht.hist(var, nbins)
-    plt.xlabel(name)
+    mean, std = np.mean(var), np.std(var)
+    print('name ', name, ': mean = ', mean, ', std = ', std)
+    #plt.figure()
+    plt.hist(var, nbins)
+    plt.xlabel(name);
 
 #
-# Drivers
+# Driver
 #-----------------
 
-def generate_wfs(xs     : np.array,
-                 ys     : np.array,
-                 zs     : np.array,
-                 enes   : np.array,
+def generate_wfs(xs     : np.array,  # x positions of the energy deposits
+                 ys     : np.array,  # y positions
+                 zs     : np.array,  # z positions
+                 enes   : np.array,  # energy of the deposits
                  histos : bool = False) -> Tuple:
+    """ Compute the WFs starting from the energy deposits
+    Inputs:
+        xs   : np.array, x positions of the energy deposits
+        ys   : np.array, y positions
+        zs   : np.array, z positions
+        enes : np.array, energy of the deposits
+    Outputs:
+        (times_pmts, wfs_pmts): (np.array (nbins), np.array (nbins, nsentors))
+            time bins of the wfs and wfs (contents) for pmt sensors
+        (times_sipms, wfs_sipms): (np.array (nbins), np.array (nbins, nsentors))
+            time bins of the wfs and wfs (contents) for sipms sensors
+    Configuration: via the detsimparams unique instance.
+        It can be updated via the configure method.
+    """
 
     t0 = time.time()
     nes = generate_electrons(enes)
@@ -331,7 +413,6 @@ def generate_wfs(xs     : np.array,
     if (histos):
         print('pes ', np.sum(pes_sipms, axis = 0))
         histo(pes_sipms, 'pes at sipms')
-
         plt.figure();
         int_pes_sipms  = np.sum(pes_sipms, axis = 0)
         plt.hist2d(detsimparams.x_sipms, detsimparams.y_sipms, bins = detsimparams.xybins, weights = int_pes_sipms);
@@ -342,7 +423,7 @@ def generate_wfs(xs     : np.array,
     if (histos):
         xcenters = times_pmts
         plt.figure()
-        plt.plot(xcenters / units.mus, wfs_pmts.T); plt.xlabel('wfs pmts')
+        plt.plot(xcenters / units.mus, wfs_pmts); plt.xlabel('wfs pmts')
         plt.xlim((np.min(dts) /units.mus - 5, np.max(dts) / units.mus + 10));
 
     # WFS SiPMs
@@ -350,10 +431,10 @@ def generate_wfs(xs     : np.array,
     if (histos):
         xcenters = times_sipms
         plt.figure()
-        plt.plot(xcenters / units.mus, wfs_sipms.T); plt.xlabel('wfs sipms')
+        plt.plot(xcenters / units.mus, wfs_sipms); plt.xlabel('wfs sipms')
         plt.xlim((np.min(dts) /units.mus - 5, np.max(dts) / units.mus + 10));
 
     t3 = time.time()
-    if (histos):
-        print('timing generate, pes, wfs', t1 - t0, t2 - t1, t3 - t2, ' s')
+    #if (histos):
+    #    print('timing generate, pes, wfs', t1 - t0, t2 - t1, t3 - t2, ' s')
     return (times_pmts, wfs_pmts), (times_sipms, wfs_sipms)
